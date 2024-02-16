ec1b4526971fc77e2b8f9402b7289e7cdeb8e3a4
Selvakumar <selvamyoor@gmail.com>
2024-02-16 10:28:10 +0530

    Working on it

---
+++ src/Homepage Components/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Carousel from "./Carousel";
+import Navbar from "./Navbar";
+
+const Homepage = () => {
+  return (
+    <div className="homePage">
+        <Navbar/>
+      <Carousel />
+    </div>
+  );
+};
+
+export default Homepage;
