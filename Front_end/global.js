6cee5fd32a0ce10f931f725faeaf990252e8d8ff
Selvakumar <selvamyoor@gmail.com>
2024-04-15 08:02:02 +0530

    user login api

--- Front_end/global.js
+++ Front_end/global.js
@@ -1,1 +1,2 @@
 export const Product_Base_Url = "https://kodai-flavors-product-api.onrender.com"
+export const User_Base_Url ='https://kodai-flavors-user-api.onrender.com'
--- Front_end/src/Data/Post Data/PostGoogleSignupData.jsx
+++ Front_end/src/Data/Post Data/PostGoogleSignupData.jsx
@@ -1,13 +1,11 @@
 import Cookies from "js-cookie";
-// import dotenv from "dotenv";
-
-// dotenv.config();
+import { User_Base_Url } from "../../../global";
 
 export const googleSinupData = async () => {
   const token = Cookies.get("gAuth");
   if (token != undefined) {
     try {
-      const userData = await fetch("http://localhost:3001/user/social/signup", {
+      const userData = await fetch(`${User_Base_Url}/user/social/signup`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ googleToken: token }),
